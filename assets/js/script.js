c5c064981e35f7d9154969a224acb153e094805e
akelstrom <akelstrom@gmail.com>
2020-11-17 13:09:29 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,7 @@ var createTask = function(taskText, taskDate, taskList) {
 
 //moment.js audit task function
 var auditTask = function(taskEl) {
+  console.log(taskEl)
   // get date from task element
   var date = $(taskEl).find("span").text().trim();
 
@@ -160,7 +161,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -188,16 +189,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    console.log("activate", this);
+    $(this).addClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag")
   },
   deactivate: function (event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag")
   },
   over: function (event) {
-    console.log("over", event.target);
+    $(this).addClass("dropover-active")
   },
   out: function (event) {
-    console.log("out", event.target);
+    $(this).removeClass("dropover-active")
   },
   update: function (event) {
     //array to store the task data in
@@ -232,12 +235,13 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-drag")
   },
   over: function(event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   out: function(event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   }
 }); //end drag and drop tasks into trash
 
@@ -255,6 +259,12 @@ $("#remove-tasks").on("click", function () {
   }
   saveTasks();
 });
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 
 // load tasks for the first time
 loadTasks();
